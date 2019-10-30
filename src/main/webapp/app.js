3248d79868d4d32e24dd883ea96b9a6e65b5f821
unknown <romil.jain@nextscm.com>
2019-10-30 20:36:01 +0530

    Added JQuery with Ajax

--- src/main/webapp/app.js
+++ src/main/webapp/app.js
@@ -1,29 +1,76 @@
 
+//BUTTON ACTIONS
 function addEmployee(event){
 	//event.preventDefault();
 	var $form = $("#employee-form");
 	var json = toJson($form);
-
-	alert("adding employee");
-
-
-	return false;
 
 	$.ajax({
 	   url: './api',
 	   type: 'POST',
 	   data: json,
 	   success: function(response) {
-	   		alert("Employee created");	     //...
+	   		console.log("Employee created");	
+	   		getEmployeeList();     //...
 	   },
 	   error: function(){
-	   		alert("En error has occurred");
+	   		alert("An error has occurred");
 	   }
 	});
 
 	return false;
 }
 
+
+function getEmployeeList(){
+	$.ajax({
+	   url: './api',
+	   type: 'GET',
+	   success: function(data) {
+	   		console.log("Employee data fetched");
+	   		console.log(data);	
+	   		displayEmployeeList(data);     //...
+	   },
+	   error: function(){
+	   		alert("An error has occurred");
+	   }
+	});
+}
+
+function deleteEmployee(id){
+	$.ajax({
+	   url: './api?id='+id,
+	   type: 'DELETE',
+	   success: function(data) {
+	   		console.log("Employee deleted");
+	   		getEmployeeList();     //...
+	   },
+	   error: function(){
+	   		alert("An error has occurred");
+	   }
+	});
+}
+
+//UI DISPLAY METHODS
+
+function displayEmployeeList(data){
+	console.log('Printing employee data');
+	var $tbody = $('#employee-table').find('tbody');
+	$tbody.empty();
+	for(var i in data){
+		var e = data[i];
+		var buttonHtml = '<button onclick="deleteEmployee(' + e.id + ')">delete</button>'
+		var row = '<tr>'
+		+ '<td>' + e.id + '</td>'
+		+ '<td>' + e.name + '</td>'
+		+ '<td>'  + e.age + '</td>'
+		+ '<td>' + buttonHtml + '</td>'
+		+ '</tr>';
+        $tbody.append(row);
+	}
+}
+
+//HELPER METHOD
 
 function toJson($form){
     var serialized = $form.serializeArray();
@@ -38,9 +85,12 @@ function toJson($form){
     return json;
 }
 
+
+//INITIALIZATION CODE
 function init(){
 	$('#add-employee').click(addEmployee);
+	$('#refresh-data').click(getEmployeeList);
 }
-
-
 $(document).ready(init);
+$(document).ready(getEmployeeList);
+
